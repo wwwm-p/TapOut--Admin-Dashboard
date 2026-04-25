cac66489065461027e0bcdadc72201ed5026d82f
wwwm-p <miap2k10@gmail.com>
2026-04-25 08:29:15 -0700

    Update login.js

--- api/auth/login.js
+++ api/auth/login.js
@@ -3,9 +3,7 @@ import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
 const { Pool } = pkg
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
-})
+const pool = new Pool({ connectionString: process.env.DATABASE_URL })
 
 export default async function handler(req, res) {
   try {
